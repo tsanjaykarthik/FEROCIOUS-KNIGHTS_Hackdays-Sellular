874fe7b046a5b57e22c483515893900454dd1d73
T. sanjay karthik <108615710+tsanjaykarthik@users.noreply.github.com>
2023-03-12 22:28:39 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBDfKgTuufNEm8jxE1gz1l1-9VUbNDeTEk",
+    authDomain: "let-s-chat-ef5d8.firebaseapp.com",
+    projectId: "let-s-chat-ef5d8",
+    storageBucket: "let-s-chat-ef5d8.appspot.com",
+    messagingSenderId: "595983029018",
+    appId: "1:595983029018:web:928737e7d8a6b79ad312a3"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
